31df89c83bad90890eb4d3e385fed1e7e46b05b3
yoecrown <jhoysner@hotmail.com>
2018-03-23 04:14:59 -0400

    Seeder para asignar jefes a acada usuario
    Seeder para asignar jefes a acada usuario y poder luego continuar con asginacion de predio

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use App\UserBoss;
 use Illuminate\Database\Seeder;
 
 class UserTableSeeder extends Seeder
@@ -12,14 +13,47 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
+        
         User::create([
            'name' => 'Admin',
            'email' => 'Admin@admin.com',
            'type_id' => '1',
            'password' => bcrypt('123456'),
+        ]);        
+
+        User::create([
+           'name' => 'yoe',
+           'email' => 'yoe@yoe.com',
+           'type_id' => 3,
+           'password' => bcrypt('123456'),
         ]);
 
-        factory(User::class, 20)->create();
+        factory(User::class, 20)->create()->each(function ($u) {
+
+              $type_id = $u->type->id;
+
+              if ($type_id <> 1 ) {
+
+                $boss_id = User::whereHas('type', function ($query) use ($type_id) {
+                              $query->where('id',$type_id-1);
+                          })->pluck('id')->random();
+
+                UserBoss::create([
+                  'user_id' => $u->id,
+                  'boss_id' => $boss_id,
+                ]);
+              }
+            
+        });
+
+        $boss_id_static = User::whereHas('type', function ($query){
+                              $query->where('id', 2);
+                          })->pluck('id')->random();
+
+        UserBoss::create([
+                  'user_id' => 2,
+                  'boss_id' => $boss_id_static,
+        ]);
 
     }
 }
